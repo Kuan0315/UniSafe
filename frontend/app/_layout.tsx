bbe3150a357526ba2aded4fb5f2f385d513081b9
Hui Yun <huiyun001126@gmail.com>
2025-09-16 16:41:09 +0800

    Add back sign in page

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -12,8 +12,8 @@ function LayoutContent() {
 
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      {/* Keep index as your auth entry point */}
-      <Stack.Screen name="index" />
+      {/* Keep login as your auth entry point */}
+      <Stack.Screen name="login" />
       {/* Only keep (tabs) if you still have app/(tabs)/_layout.tsx */}
       <Stack.Screen name="(tabs)" />
     </Stack>
